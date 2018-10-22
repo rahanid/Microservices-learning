b12a717227daf16f78d001f23be9146d74fbf119
Dinahar V <rahanid@gmail.com>
2018-10-22 15:56:55 +0530

    Update my-app/src/main/java/com/example/demo/MyAppApplication.java

--- my-app/src/main/java/com/example/demo/MyAppApplication.java
+++ my-app/src/main/java/com/example/demo/MyAppApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,13 +18,19 @@ public class MyAppApplication {
 	
 	/*@Autowired
 	private RestTemplate restTemplate;*/
+	
+	public static final Logger LOGGER = LoggerFactory.getLogger(MyAppApplication.class);
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyAppApplication.class, args);
 	}
 	
+	
+	
 	 @GetMapping("/check")
 	    public String greeting() {
+		 LOGGER.info("my app is up");
 	        return "my app is up";
 	    }
 	 
